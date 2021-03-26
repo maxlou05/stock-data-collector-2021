532804056f4b3500cf20c7c6a98c0ec91997f10d
maxlou05 <62454180+maxlou05@users.noreply.github.com>
2021-03-25 21:24:19 -0400

    Update stock_data.py

--- stock_data.py
+++ stock_data.py
@@ -164,7 +164,7 @@ for i in range((PORTION-1)*PORTION_SIZE, LIMIT):
     
     spaces = " "*(8-len(s.TICKER))
     percent = round((i+1)/LIMIT, 4)*100
-    print("data retrival complete for: {}{}{}%    ({}/{})".format(s.TICKER, spaces, percent, i+1, LIMIT), end='\n', flush=True)
+    print("data retrival complete for: {}{}{}%    ({}/{})".format(s.TICKER, spaces, percent, i+1, LIMIT), end='\n', flush=True)  # Adding end and flush ensures it doesn't get stuck buffering sometimes.
 
 if OUT_OF_CALLS:
     print("--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---")
